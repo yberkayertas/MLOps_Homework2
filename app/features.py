e3d922f9158c89b4a717d4f3be6a44c53012f458
Berkay Ertaş <ysufbrkay@gmail.com>
2026-01-09 15:03:09 +0300

    FINAL3

--- app/features.py
+++ app/features.py
@@ -8,7 +8,4 @@ def get_hashing_index(input_string: str, num_buckets: int = 1000):
     hash_object = hashlib.md5(input_string.encode())
     return int(hash_object.hexdigest(), 16) % num_buckets
 
-# #Sabotage
-# def get_hashing_index(input_string: str, num_buckets: int = 1000):
-#     return (int(hashlib.md5(input_string.encode()).hexdigest(), 16) % num_buckets) + 99999
 
