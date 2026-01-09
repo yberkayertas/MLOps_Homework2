fd30a97bf423b2b167a08fdaed9d96bf7469cfc6
Berkay Ertaş <ysufbrkay@gmail.com>
2026-01-09 15:00:15 +0300

    FINAL

--- app/features.py
+++ app/features.py
@@ -1,14 +1,15 @@
 import hashlib
 
 
-# def get_hashing_index(input_string: str, num_buckets: int = 1000):
-#     """High-cardinality veriler için hashing logic."""
-#     if not isinstance(input_string, str):
-#         input_string = str(input_string)
-#     hash_object = hashlib.md5(input_string.encode())
-#     return int(hash_object.hexdigest(), 16) % num_buckets
-
 def get_hashing_index(input_string: str, num_buckets: int = 1000):
-    # (Sabotaj)
-    return (int(hashlib.md5(input_string.encode()).hexdigest(), 16) % num_buckets) + 99999
+    """High-cardinality veriler için hashing logic."""
+    if not isinstance(input_string, str):
+        input_string = str(input_string)
+    hash_object = hashlib.md5(input_string.encode())
+    return int(hash_object.hexdigest(), 16) % num_buckets
+
+# #Sabotage
+# def get_hashing_index(input_string: str, num_buckets: int = 1000):
+    
+#     return (int(hashlib.md5(input_string.encode()).hexdigest(), 16) % num_buckets) + 99999
 
